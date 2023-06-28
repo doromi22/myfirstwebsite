ca52b26d5ad747bba8021d2d69d299d8c47a3580
doromi22 <128030698+doromi22@users.noreply.github.com>
2023-06-28 14:49:04 +0900

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-session_destroy();
-
-// logout and redirect to index file
-echo '<script>alert("logged out successfully."); window.location.href = "index.php";</script>';
-exit();
-?>
